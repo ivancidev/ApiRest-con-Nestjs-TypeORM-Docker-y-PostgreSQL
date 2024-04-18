739a271723254367851a3b3b3920120a6e53ba01
Herlan <herlanherbas@gmail.com>
2024-04-17 21:02:13 -0400

    feature(Documented with swagger in location)

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { LocationService } from './location.service';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Location')
 @Controller('location')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
