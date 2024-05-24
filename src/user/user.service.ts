caeb7b4723c5e0fc1733ff1c679ad17640a4b0ae
Herlan <herlanherbas@gmail.com>
2024-05-24 08:10:56 -0400

    add: findUserByEmail and relations

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -47,12 +47,12 @@ export class UserService {
     }
   }
 
-  findAll() {
-    return this.userRepository.find();
+  async findAll() {
+    return await this.userRepository.find({ withDeleted: true });
   }
 
   async findOne(id: string) {
-    const user = await this.userRepository.findOne({ where: { id } });
+    const user = await this.userRepository.findOne({ where: { id }, relations: ['accessLog'] });
 
     if (!user) {
       throw new NotFoundException(`No user found with id: ${id}`);
@@ -66,6 +66,8 @@ export class UserService {
     if (!user) {
       throw new NotFoundException(`No user found with id: ${id}`);
     }
+    // user.accessLog.map() si no funciona con cascade aplicar este metodo
+
     user.email = updateUserDto.email || user.email;
     user.name = updateUserDto.name || user.name;
     user.password = updateUserDto.password || user.password;
@@ -73,6 +75,16 @@ export class UserService {
     user.code = updateUserDto.code || user.code;
 
     return this.userRepository.save(user);
+  }
+
+  async findUserByEmail(email: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException(`No user found with email: ${email}`);
+    }
+
+    return user;
   }
 
   async remove(id: string) {
